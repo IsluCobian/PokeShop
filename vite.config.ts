6bc007717decfd49610b899c4c01c5640eb9c777
Islu <luigui.angel07@hotmail.com>
2025-02-04 17:39:33 -0600

    fix: base path on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/PokeShop/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
