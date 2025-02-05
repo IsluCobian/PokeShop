63b8d744918815e046ee1a01aab453105ed86230
Islu <luigui.angel07@hotmail.com>
2025-02-04 20:00:53 -0600

    adjust layout and sizing of CartItem component for better responsiveness

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -8,24 +8,26 @@ export default function CartItem({ item }: { item: CartItem }) {
       <img
         src={item.image}
         alt={item.name}
-        className="w-20 h-20 object-cover bg-card p-2 rounded-lg"
+        className="w-26 h-26 md:w-20 md:h-20 object-cover bg-card p-2 rounded-lg"
       />
       <div className="flex items-center w-full justify-between">
         <div className="ml-4 text-left w-full">
           <p className="text-lg font-semibold capitalize">{item.name}</p>
-          <div className="flex items-center justify-between w-full">
-            <p className="text-lg font-semibold flex flex-col text-left">
-              <span className=" font-light text-muted-foreground text-sm">
-                Precio Unitario
-              </span>
-              ${item.price.toFixed(2)}
-            </p>
-            <p className="text-lg font-semibold flex flex-col text-left">
-              <span className=" font-light text-sm text-muted-foreground">
-                Total Parcial
-              </span>
-              ${(item.price * item.quantity).toFixed(2)}
-            </p>
+          <div className="flex items-start md:items-center justify-between w-full flex-col md:flex-row">
+            <div className="flex items-center justify-between gap-4 w-full md:w-1/2 mb-2">
+              <p className="text-lg font-semibold flex flex-col text-left">
+                <span className=" font-light text-muted-foreground text-sm">
+                  Precio Unitario
+                </span>
+                ${item.price.toFixed(2)}
+              </p>
+              <p className="text-lg font-semibold flex flex-col text-left">
+                <span className=" font-light text-sm text-muted-foreground">
+                  Total Parcial
+                </span>
+                ${(item.price * item.quantity).toFixed(2)}
+              </p>
+            </div>
             <div className="flex items-center gap-4">
               <CartItemQuantityInput item={item} />
               <RemoveFromCartButton item={item} />
