1745dc69b4b0f51230c43bf39914d6f08587f7f6
Islu <luigui.angel07@hotmail.com>
2025-02-04 18:07:45 -0600

    fix: update paths for favicon and main script in HTML and Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
     <header className=" relative flex flex-col md:flex-row gap-2 md:gap-0 items-center justify-between container mx-auto">
       <Link to="/">
         <img
-          src="./pokeshop.webp"
+          src="pokeshop.webp"
           alt="PokéShop"
           className="h-16 lg:h-20 object-contain max-w-md md:ml-2"
         />
